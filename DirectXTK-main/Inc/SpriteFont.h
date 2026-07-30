c14f4b117e5b48ba622f6d61b21908ef9cc8b5df
tanaka-970 <tanaka.hiroto.1@icloud.com>
2026-07-30 10:54:23 +0900

    DirectXTKのバージョンを2026に変更

--- DirectXTK-main/Inc/SpriteFont.h
+++ DirectXTK-main/Inc/SpriteFont.h
@@ -4,7 +4,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 //
-// http://go.microsoft.com/fwlink/?LinkId=248929
+// https://go.microsoft.com/fwlink/?LinkId=248929
 //--------------------------------------------------------------------------------------
 
 #pragma once
@@ -144,7 +144,9 @@ namespace DirectX
 
             // Spacing properties
             DIRECTX_TOOLKIT_API float __cdecl GetLineSpacing() const noexcept;
-            DIRECTX_TOOLKIT_API void __cdecl SetLineSpacing(float spacing);
+            DIRECTX_TOOLKIT_API void __cdecl SetLineSpacing(float spacing) noexcept;
+
+            DIRECTX_TOOLKIT_API void __cdecl SetPixelAlignment(bool enable) noexcept;
 
             // Font properties
             DIRECTX_TOOLKIT_API wchar_t __cdecl GetDefaultCharacter() const noexcept;
