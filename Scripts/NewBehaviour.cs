27ba622c9a661972c8c55b64f390f004d1932c49
青山田 <pikmin20222022@gmail.com>
2026-08-04 00:44:33 +0900

    fukushima_dousa_kensyouを追加。 （コード内容自体は、わざとエラーを起こす様にしています。 ただし、実行は出来ています。）

---
+++ Scripts/NewBehaviour.cs
@@ -0,0 +1,18 @@
+using ReplayEngine;
+
+namespace Game;
+
+[ReplayGuid("205ccf78170532b2f086c75618c64235")]
+public sealed class NewBehaviour : ScriptBehaviour
+{
+    public float Speed = 1.0f;
+    public ObjectReference Target;
+
+    public override void Awake()
+    {
+    }
+
+    public override void Update(float deltaTime)
+    {
+    }
+}
