3dd28f38cd0f729afb7d1a5184a106bfd9a0b353
青山田 <pikmin20222022@gmail.com>
2026-09-02 18:29:30 +0900

    テストでのデータ変更

---
+++ .ReplayTrash/NewItem.cs
@@ -0,0 +1,18 @@
+using ReplayEngine;
+
+namespace Game;
+
+[ReplayGuid("e109e0eea27cfdf02f5901a1b509b5e8")]
+public sealed class NewItem : ScriptBehaviour
+{
+    public float Speed = 1.0f;
+    public ObjectReference Target;
+
+    public override void Awake()
+    {
+    }
+
+    public override void Update(float deltaTime)
+    {
+    }
+}
